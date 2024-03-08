87eaf807d0a3685690202f9c927523b60bf26be6
huoyijie <yijie.huo@foxmail.com>
2024-03-08 17:00:57 +0800

    refactor crud statement def

--- orm.go
+++ orm.go
@@ -2,36 +2,37 @@ package orm
 
 import (
 	"github.com/authink/orm/model"
+	"github.com/authink/orm/sql"
 	"github.com/jmoiron/sqlx"
 )
 
-type Inserter[T any] interface {
-	Insert(*T) error
-	InsertTx(*sqlx.Tx, *T) error
+type Inserter[M model.Identifier] interface {
+	Insert(M) error
+	InsertTx(*sqlx.Tx, M) error
 }
 
-type Saver[T any] interface {
-	Save(*T) error
-	SaveTx(*sqlx.Tx, *T) error
+type Saver[M model.Identifier] interface {
+	Save(M) error
+	SaveTx(*sqlx.Tx, M) error
 }
 
-type Updater[T any] interface {
-	Update(*T) error
-	UpdateTx(*sqlx.Tx, *T) error
+type Updater[M model.Identifier] interface {
+	Update(M) error
+	UpdateTx(*sqlx.Tx, M) error
 }
 
-type Geter[T any] interface {
-	Get(*T) error
-	GetTx(*sqlx.Tx, *T) error
+type Geter[M model.Identifier] interface {
+	Get(M) error
+	GetTx(*sqlx.Tx, M) error
 }
 
-type Deleter[T any] interface {
-	Delete(*T) error
-	DeleteTx(*sqlx.Tx, *T) error
+type Deleter[M model.Identifier] interface {
+	Delete(M) error
+	DeleteTx(*sqlx.Tx, M) error
 }
 
-type Finder[T any] interface {
-	Find(...model.Arg) ([]T, error)
+type Finder[M model.Identifier] interface {
+	Find(...model.Arg) ([]M, error)
 }
 
 type Counter interface {
@@ -39,6 +40,92 @@ type Counter interface {
 	CountTx(*sqlx.Tx, ...model.Arg) (int, error)
 }
 
-type Pager[T any] interface {
-	PaginationTx(*sqlx.Tx, model.Pager) ([]T, error)
+type Pager[M model.Identifier] interface {
+	PaginationTx(*sqlx.Tx, model.Pager) ([]M, error)
+}
+
+type ORM[M model.Identifier] interface {
+	Inserter[M]
+	Saver[M]
+	Updater[M]
+	Geter[M]
+	Deleter[M]
+	Finder[M]
+	Counter
+	Pager[M]
+}
+
+type ORMBase[M model.Identifier, S sql.SQL] struct {
+	Executor
+	Stmt S
+}
+
+func (o *ORMBase[M, S]) Insert(m M) error {
+	return NamedInsert(o.Executor, o.Stmt.Insert(), m)
+}
+
+func (o *ORMBase[M, S]) InsertTx(tx *sqlx.Tx, m M) error {
+	return NamedInsert(tx, o.Stmt.Insert(), m)
+}
+
+func (o *ORMBase[M, S]) Save(m M) error {
+	return NamedSave(o.Executor, o.Stmt.Save(), m)
+}
+
+func (o *ORMBase[M, S]) SaveTx(tx *sqlx.Tx, m M) error {
+	return NamedSave(tx, o.Stmt.Save(), m)
+}
+
+func (o *ORMBase[M, S]) Update(m M) error {
+	return NamedUpdate(o.Executor, o.Stmt.Update(), m)
+}
+
+func (o *ORMBase[M, S]) UpdateTx(tx *sqlx.Tx, m M) error {
+	return NamedUpdate(tx, o.Stmt.Update(), m)
+}
+
+func (o *ORMBase[M, S]) Get(m M) error {
+	return Get(o.Executor, o.Stmt.Get(), m)
+}
+
+func (o *ORMBase[M, S]) GetTx(tx *sqlx.Tx, m M) error {
+	return Get(tx, o.Stmt.GetForUpdate(), m)
+}
+
+func (o *ORMBase[M, S]) Delete(m M) error {
+	return Delete(o.Executor, o.Stmt.Delete(), m)
+}
+
+func (o *ORMBase[M, S]) DeleteTx(tx *sqlx.Tx, m M) error {
+	return Delete(tx, o.Stmt.Delete(), m)
+}
+
+func (o *ORMBase[M, S]) Find(args ...model.Arg) (list []M, err error) {
+	err = Select(o.Executor, o.Stmt.Find(), &list, o.parseArg(args))
+	return
+}
+
+func (o *ORMBase[M, S]) Count(args ...model.Arg) (c int, err error) {
+	err = Count(o.Executor, o.Stmt.Count(), &c, o.parseArg(args))
+	return
+}
+
+func (o *ORMBase[M, S]) CountTx(tx *sqlx.Tx, args ...model.Arg) (c int, err error) {
+	err = Count(tx, o.Stmt.Count(), &c, o.parseArg(args))
+	return
+}
+
+func (o *ORMBase[M, S]) PaginationTx(tx *sqlx.Tx, pager model.Pager) (list []M, err error) {
+	err = Select(tx, o.Stmt.Pagination(), &list, pager)
+	return
+}
+
+func (o *ORMBase[M, S]) parseArg(args []model.Arg) model.Arg {
+	var arg model.Arg
+	if len(args) > 0 {
+		arg = args[0]
+	} else {
+		arg = &model.Argument{}
+	}
+	return arg
 }
--- sql/crud.go
+++ sql/crud.go
@@ -35,3 +35,64 @@ type Counter interface {
 type Pager interface {
 	Pagination() string
 }
+
+type SQL interface {
+	Inserter
+	Saver
+	Deleter
+	Updater
+	Geter
+	GeterForUpdate
+	Finder
+	Counter
+	Pager
+}
+
+type SQLBase struct{}
+
+// Count implements SQL.
+func (s *SQLBase) Count() string {
+	panic("unimplemented")
+}
+
+// Delete implements SQL.
+func (s *SQLBase) Delete() string {
+	panic("unimplemented")
+}
+
+// Find implements SQL.
+func (s *SQLBase) Find() string {
+	panic("unimplemented")
+}
+
+// Get implements SQL.
+func (s *SQLBase) Get() string {
+	panic("unimplemented")
+}
+
+// GetForUpdate implements SQL.
+func (s *SQLBase) GetForUpdate() string {
+	panic("unimplemented")
+}
+
+// Insert implements SQL.
+func (s *SQLBase) Insert() string {
+	panic("unimplemented")
+}
+
+// Pagination implements SQL.
+func (s *SQLBase) Pagination() string {
+	panic("unimplemented")
+}
+
+// Save implements SQL.
+func (s *SQLBase) Save() string {
+	panic("unimplemented")
+}
+
+// Update implements SQL.
+func (s *SQLBase) Update() string {
+	panic("unimplemented")
+}
+
+var _ SQL = (*SQLBase)(nil)
