f1855656886ee535731b569430399f2247236329
huoyijie <yijie.huo@foxmail.com>
2024-03-08 17:46:51 +0800

    refactor crud statement def

--- executor.go
+++ executor.go
@@ -2,6 +2,7 @@ package orm
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/authink/orm/model"
 	"github.com/jmoiron/sqlx"
@@ -64,4 +65,36 @@ func prepareNamed(executor Executor, statement string, dest, arg any, isSelect b
 		err = stmt.Get(dest, arg)
 	}
 	return
+}
+
+func afterInsert(result sql.Result, m model.Identifier) (err error) {
+	lastId, err := result.LastInsertId()
+	if err != nil {
+		return
+	}
+
+	m.SetId(uint32(lastId))
+	return
+}
+
+func afterSave(result sql.Result, m model.Identifier) (err error) {
+	if err = afterInsert(result, m); err != nil {
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return
+	} else if rowsAffected == 0 {
+		err = errors.New("duplicate key")
+	}
+	return
+}
+
+func afterUpdate(sql.Result, model.Identifier) error {
+	return nil
+}
+
+func afterDelete(sql.Result, model.Identifier) error {
+	return nil
 }
